031523a92b930085b6d25a82fe8ba2c2cb309eef
thisisrealcd <cdzaa9850@gmail.com>
2026-02-27 00:22:21 +0700

    Add dentist function to routes

---
+++ routes/dentists.js
@@ -0,0 +1,38 @@
+const express = require("express");
+
+const {
+  getDentist,
+  getDentists,
+  createDentist,
+  updateDentist,
+  deleteDentist
+} = require("../controllers/dentists");
+
+const bookingRouter = require("./bookings");
+
+const router = express.Router();
+
+const { protect, authorize } = require("../middleware/auth");
+
+// Nested route
+// /api/v1/dentists/:dentistId/bookings
+router.use('/:dentistId/bookings', bookingRouter);
+
+
+// GET all dentists
+// POST create dentist (admin only)
+router.route('/')
+  .get(getDentists)
+  .post(protect, authorize('admin'), createDentist);
+
+
+// GET single dentist
+// PUT update dentist (admin only)
+// DELETE dentist (admin only)
+router.route('/:id')
+  .get(getDentist)
+  .put(protect, authorize('admin'), updateDentist)
+  .delete(protect, authorize('admin'), deleteDentist);
+
+
+module.exports = router;
