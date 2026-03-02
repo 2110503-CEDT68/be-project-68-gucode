31922fbaaed60856d8c0db355c3a49953e5120cc
Saranyaphong <saranyaphong.toeiphutsa@gmail.com>
2026-03-02 14:44:38 +0700

    initial fix

--- models/Booking.js
+++ models/Booking.js
@@ -1,33 +1,30 @@
 const mongoose = require('mongoose');
 
-const bookingSchema = new mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
-  },
-  dentist: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Dentist',
-    required: true
-  },
-  date: {
-    type: Date,
-    required: [true, 'Please provide a booking date'],
-    validate: {
-      validator: function(value) {
-        return value >= new Date();
-      },
-      message: 'Booking date must be in the future'
+const BookingSchema = new mongoose.Schema({
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    dentist:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Dentist',
+        required: true
+    }, 
+    date: {
+        type: Date,
+        required: [true, 'Please provide a booking date'],
+        validate: {
+            validator: function(value) {
+                return value >= new Date();
+            },
+            message: 'Booking date must be in the future'
+        }
+    },
+    createdAt:{
+        type: Date,
+        default: Date.now
     }
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
-  }
 });
 
-// Index to ensure one active booking per user
-bookingSchema.index({ user: 1, status: 1 });
-
-module.exports = mongoose.model('Booking', bookingSchema);
+module.exports = mongoose.model('Booking', BookingSchema);
